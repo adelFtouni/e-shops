5ed5b128e4e6b2cfbcb9994f19f054e89a8c700b
adelFtouni <101456560+adelFtouni@users.noreply.github.com>
2025-11-02 10:30:18 +0300

    Change repository name in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Replace 'REPO' with your repo name, or use '/' for user pages
-const repoName = "e-shops-frontend";
+const repoName = "e-shops";
 const basePath = `/${repoName}/`;
 
 export default defineConfig({
